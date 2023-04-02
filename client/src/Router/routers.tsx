bdfb1f10ebee77d6e690dd262eb5eb70d19eee8c
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2023-04-02 21:48:31 +0700

    Tạo pj note app bằng react và node

---
+++ client/src/Router/routers.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </div>
+  );
+};
+
+export default Router;
