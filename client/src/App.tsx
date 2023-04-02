64e2cd025caf683eb9c1cec7d95bf9666ce91a9b
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2023-04-02 23:08:07 +0700

    setup môi trường node js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import 'antd/dist/reset.css';
-import './App.css';
 
 function App() {
   return <div className="App"></div>;
---
+++ server/src/index.mjs
@@ -0,0 +1,30 @@
+import express from 'express';
+import morgan from 'morgan';
+import http from 'http';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+
+const app = express();
+const httpServer = http.createServer(app);
+const PORT = process.env.PORT || 4000;
+// Connect to DB
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@note-app.e8a6igu.mongodb.net/?retryWrites=true&w=majority`;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.json());
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    console.log('Connected to DB');
+    await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+    console.log(`🚀 Server ready at http://localhost:${PORT}`);
+  });
