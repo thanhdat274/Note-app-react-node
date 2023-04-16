b85ae5fb65a3b9212eaf9df2b244ac9598f3108b
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2023-04-16 17:38:45 +0700

    update api

--- server/index.js
+++ server/index.js
@@ -1,38 +1,46 @@
-import express from 'express';
 import morgan from 'morgan';
-import http from 'http';
-import cors from 'cors';
-import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+
+
 import homeRouter from './src/router/home';
 import userRouter from './src/router/auth';
 import folderRouter from './src/router/folder.router';
 import noteRouters from './src/router/notes.router';
 
-const app = express();
-const httpServer = http.createServer(app);
-const PORT = process.env.PORT || 4000;
-// Connect to DB
-const URI = process.env.MONGODB_URL;
+const mongoString = process.env.MONGODB_URL;
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useUnifiedTopology', true);
 
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error);
+});
+
+database.once('connected', () => {
+  console.log('Database Connected');
+});
+
+const app = express();
 app.use(cors());
-app.use(bodyParser.json());
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(homeRouter);
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(express.json({ limit: '20mb' }));
 app.use('/api', userRouter)
 app.use('/api', folderRouter)
 app.use('/api', noteRouters)
 
-mongoose.set('strictQuery', false);
-mongoose
-  .connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(async () => {
-    console.log('Connected to DB');
-    await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
-    console.log(`🚀 Server ready at http://localhost:${PORT}`);
-  });
+app.listen(8800, () => {
+  console.log(`Server Started at ${8800}`);
+});
