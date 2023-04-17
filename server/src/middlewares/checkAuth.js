b6e63e744311458d518e5a83f15e783c099cce3e
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2023-04-17 22:20:04 +0700

    update

--- server/src/middlewares/checkAuth.js
+++
@@ -1,7 +0,0 @@
-const { expressjwt: expressJwt } = require("express-jwt");
-
-export const requireSignin = expressJwt({
-  algorithms: ["HS256"],
-  secret: "123456",
-  requestProperty: "auth", // req.auth
-});
