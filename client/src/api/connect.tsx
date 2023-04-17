9b4682fb674618bd39e0dce3b6b4e63c271bb3c4
Nguyễn Thành Đạt <nguyenthanhdat27042002@gmail.com>
2023-04-17 22:50:07 +0700

    update

--- client/src/api/connect.tsx
+++ client/src/api/connect.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const connect = axios.create({
-  baseURL: 'https://note-app-react-node-y78v.vercel.app/api',
+  baseURL: 'https://note-app-react-node-z44g.vercel.app/api',
 });
